320964719804902a2f275c178a8b406550536ceb
Fiona <fiona.valencia.chen@gmail.com>
2022-07-25 15:37:48 +0700

    initial project

---
+++ nano 2 Fiona/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nano 2 Fiona
+//
+//  Created by Fiona Valencia on 25/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ nano 2 Fiona/nano_2_FionaApp.swift
@@ -0,0 +1,17 @@
+//
+//  nano_2_FionaApp.swift
+//  nano 2 Fiona
+//
+//  Created by Fiona Valencia on 25/07/22.
+//
+
+import SwiftUI
+
+@main
+struct nano_2_FionaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
